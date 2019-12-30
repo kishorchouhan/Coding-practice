31f8f75892a6e782e5b8905313e1584b5155c1ab
kishorchouhan <devkishorbwr@gmail.com>
2019-12-30 20:03:07 +0530

    time convert solution

---
+++ coderbyte.com/timeConvert.js
@@ -0,0 +1,22 @@
+// Time Convert
+
+// Have the function TimeConvert(num) take the num parameter being passed and
+// return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3).
+// Separate the number of hours and minutes with a colon.
+
+// Examples:
+
+// Input: 126
+// Output: 2:6
+
+// Input: 45
+// Output: 0:45
+
+function TimeConvert(num) {
+  let hour = Math.floor(num / 60);
+  let min = num % 60;
+  return `${hour}:${min}`;
+}
+
+// keep this function call here
+console.log(TimeConvert(8));
