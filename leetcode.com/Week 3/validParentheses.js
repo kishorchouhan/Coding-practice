76735ab0b89b86e94156b0f4eb77005afe6b9905
kishorchouhan <devkishorbwr@gmail.com>
2021-04-03 01:28:13 +0530

    20. Valid Parentheses done

--- leetcode.com/Week 3/validParentheses.js
+++ leetcode.com/Week 3/validParentheses.js
@@ -51,8 +51,6 @@ var isValid = function(s) {
         "[": "]",
         "{": "}"
     }
-    let openB = "([{";
-    let closeB = ")]}";
     let lastOpen = ""
     for(let i = 0; i < s.length ; i++){
         let currentChar = s[i];
@@ -69,4 +67,50 @@ var isValid = function(s) {
         }
     }
     return lastOpen.length === 0 ? true : false;
+};
+
+var isValid = function(s) {
+
+    let brackets = new Map()
+    brackets.set("(", ")")
+    brackets.set("[", "]")
+    brackets.set("{", "}")
+
+    let lastOpen = ""
+    for(let i = 0; i < s.length ; i++){
+        let currentChar = s[i];
+        let currentCharType = brackets.has(currentChar) ? "open" : "close";
+
+        if(currentCharType === "open"){
+            lastOpen += currentChar;
+        }else if(currentCharType === "close"){
+            if(brackets.get(lastOpen[lastOpen.length - 1]) === currentChar){
+                lastOpen = lastOpen.slice(0, -1);
+            }else{
+                return false;
+            }
+        }
+    }
+    return lastOpen.length === 0 ? true : false;
+};
+
+// Runtime: 80 ms, faster than 68.87% of JavaScript online submissions for Valid Parentheses.
+// Memory Usage: 38.6 MB, less than 91.95% of JavaScript online submissions for Valid Parentheses.
+var isValid = function(s) {
+    let brackets = new Map()
+    brackets.set(")", "(")
+    brackets.set("]", "[")
+    brackets.set("}", "{")
+
+    let lastStack = [];
+    for(let i = 0; i < s.length ; i++){
+        let currentChar = s[i];
+        if(brackets.has(currentChar)){
+            let topEle = lastStack.length ? lastStack.pop() : "#";
+            if(topEle !== brackets.get(currentChar)) return false;
+        }else{
+            lastStack.push(currentChar)
+        }
+    }
+    return lastStack.length === 0 ? true : false;
 };
